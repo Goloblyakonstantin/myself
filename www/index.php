066388570bcbdbdeb1071e20c93e67578924f44b
goloblyakonstantin <goloblyakonstantin@gmail.com>
2020-03-14 06:21:52 +0300

    звезды деревья старт скрипта высота для мобильных

--- www/index.php
+++ www/index.php
@@ -7,6 +7,14 @@
     <title></title>
   </head>
   <body>
+    <?php
+      for ($i=0; $i <17 ; $i++)
+      {
+        $x=rand(0,100);
+        $y=rand(0,100);
+        echo '<img src="img/star.png" style="left:'.$x.'vw; top:'.$y.'vh" class="star" alt="">';
+      }
+    ?>
     <img src="img/moon.svg" class="moon" alt="">
     <img src="img/mountain_1.png" class="mountain_1" alt="">
     <img src="img/land_2_2.png" class="land_2" alt="">
@@ -14,7 +22,14 @@
     <img src="img/mountain_0.png" class="mountain_0" alt="">
     <img src="img/land_0_0.png" class="land_0" alt="">
     <img src="img/tree_0.png" class="tree_0" alt="">
-
+    <?php
+      for ($i=0; $i <5 ; $i++)
+      {
+        $x=rand(0,100);
+        $width=rand(10,35);
+        echo '<img src="img/tree_0.png" style="left:'.$x.'vw; width:'.$width.'vw" class="tree_0" alt="">';
+      }
+    ?>
     <script src="https://code.jquery.com/jquery-3.4.1.js"
     integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
     crossorigin="anonymous"></script>
--- www/js/js.js
+++ www/js/js.js
@@ -6,13 +6,30 @@ body_height=body_height-body_height/(height_y+1)
 var min_x=body_width/100;
 var min_y=body_height/100;
 
-setTimeout(start,1000)
+//для мобильных
+if ($(window).width()<$(window).height())
+{
+    $('html, body').height($('html, body').height()/7*4)
+    height_y=height_y/2;
+    body_width=$('html, body').width();
+    body_height=$('html, body').height();
+    body_height=body_height-body_height/(height_y+1)
+    min_x=body_width/100;
+    min_y=body_height/100;
+    move();
+}
+
+//после прелоадера
+$(document).ready(function()
+{
+  setTimeout(start,1000)
+});
 
 //стартуем
 function start()
 {
-  $(window).scrollTop(0);
   move();
+  $(window).scrollTop(0);
 }
 
 //скролим
@@ -26,7 +43,6 @@ $(window).resize(function()
 {
   body_width=$('html, body').width();
   body_height=$('html, body').height();
-  height_y=6;
   body_height=body_height-body_height/(height_y+1)
   min_x=body_width/100;
   min_y=body_height/100;
@@ -45,5 +61,10 @@ function move()
   $('.land_2').offset({top:scroll_top+100*min_y/height_y-$('.land_2').height()+60*min_y/height_y*(body_height-scroll_top)/body_height});
   $('.mountain_0').offset({top:scroll_top+100*min_y/height_y-$('.mountain_0').height()+140*min_y/height_y*(body_height-scroll_top)/body_height});
   $('.mountain_1').offset({top:scroll_top+100*min_y/height_y-$('.mountain_1').height()+60*min_y/height_y*(body_height-scroll_top)/body_height});
-  $('.tree_0').offset({top:scroll_top+100*min_y/height_y-$('.tree_0').height()+210*min_y/height_y*(body_height-scroll_top)/body_height});
+  var tree=$('.tree_0');
+  for (var i = 0; i < $('.tree_0').length; i++)
+  {
+    $(tree[i]).offset({top:scroll_top+100*min_y/height_y-$(tree[i]).height()*0.9+210*min_y/height_y*(body_height-scroll_top)/body_height});
+  }
+
 }
